6f389ce1bc9f966970db41b821904a3ca3c7b2bd
André <andremendesfelix@gmail.com>
2023-05-25 22:26:25 -0300

    Criando imagens e executáveis Java nativos com GraalVM

---
+++ src/test/java/com/example/graalvmnativeimages/GraalvmNativeImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graalvmnativeimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmNativeImagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
